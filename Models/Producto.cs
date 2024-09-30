087c97de4ee35ede4f4a21ae23fd2ae3beb5ec28
Amy Cherrez <amy.cherrez@udla.edu.ec>
2024-09-30 12:11:46 -0500

    Se creo la tabla Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_AC.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
